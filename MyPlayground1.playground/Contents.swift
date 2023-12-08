6befe10969f803febbc2622478563e4b059bfa50
houhoz <houyazhao@gmail.com>
2023-12-09 00:14:39 +0800

    first commit

---
+++ MyPlayground1.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+print(quote.count)
+
+print(quote.uppercased())
+
+// 多行
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+print(movie.hasPrefix("A day"))
+
+let filename = "paris.jpg"
+print(filename.hasSuffix(".jpg"))
+
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+
+print(120.isMultiple(of: 3))
+
+
+let number1 = 0.1 + 0.2
+print(number1)
+
+
+let a = 1
+let b = 2.0
+let c = a + Int(b)
+let c1 = Double(a) + b
