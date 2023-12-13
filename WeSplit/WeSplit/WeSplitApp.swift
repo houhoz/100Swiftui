c7498b8350bf04a1181040eea4f40eb03aeb7f03
houyazhao_v <houyazhao_v@didiglobal.com>
2023-12-13 14:27:51 +0800

    feat: WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by didi on 2023/12/13.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
