00822d4ebb317bc7a858f9e2582a12959c1c2261
houhoz <houyazhao@gmail.com>
2023-12-15 23:15:04 +0800

    Day 16 – Project 1, part one

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -5,17 +5,51 @@
 //  Created by didi on 2023/12/13.
 //
 
+// 包含程序的初始用户界面（UI），我们将在此项目中完成所有工作。
+
 import SwiftUI
 
 struct ContentView: View {
+//    @State private var tapCount = 0
+//    @State private var name = ""
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!!! 很好的话")
-        }
-        .padding()
+        Text("Hello, world!")
+//        NavigationStack {
+//               Form {
+//                   Picker("Select your student", selection: $selectedStudent) {
+//                       ForEach(students, id: \.self) {
+//                           Text($0)
+//                       }
+//                   }
+//               }
+//        }
+//        Button("Tap Count: \(tapCount)") {
+//            self.tapCount += 1
+//        }
+//        Form {
+//            ForEach(0..<100) { number in
+//                Text("Row \(number)")
+//            }
+//        }
+//        Form {
+//            ForEach(0 ..< 100) {
+//                Text("Row \($0)")
+//            }
+//        }
+//        NavigationStack {
+//            Form {
+//                Section {
+//                    Text("Hello, world!")
+//                }
+//                TextField("Enter your name", text: $name)
+//                Text("Your name is \(name)")
+//            }
+//            .navigationTitle("SwiftUI")
+//            .navigationBarTitleDisplayMode(.inline)
+//        }
     }
 }
 
--- WeSplit/WeSplit/WeSplitApp.swift
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -5,6 +5,10 @@
 //  Created by didi on 2023/12/13.
 //
 
+// WeSplitApp.swift 包含用于启动应用程序的代码。如果您在应用程序启动时创建了一些内容，并使其始终保持激活状态，则应将其放在此处。
+//Assets.xcassets 是一个资产目录，收集了你想在应用中使用的图片。您还可以在这里添加颜色以及应用图标、iMessage 贴纸等。
+//Preview Content（预览内容）是一个组，其中包含 Preview Assets.xcassets--这是另一个资产目录，这次是专门为你在设计用户界面时要使用的示例图片而设，让你了解程序运行时这些图片会是什么样子。
+
 import SwiftUI
 
 @main
