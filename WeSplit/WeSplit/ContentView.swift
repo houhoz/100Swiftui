8c847d71f1427bbdec500872b157acd8810cefd6
houhoz <houyazhao@gmail.com>
2023-12-16 23:35:20 +0800

    Project 1

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -1,55 +1,62 @@
-//
-//  ContentView.swift
-//  WeSplit
-//
-//  Created by didi on 2023/12/13.
-//
-
-// 包含程序的初始用户界面（UI），我们将在此项目中完成所有工作。
-
 import SwiftUI
 
 struct ContentView: View {
-//    @State private var tapCount = 0
-//    @State private var name = ""
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    let tipPercentages = [10, 15, 20, 25, 0]
+    @FocusState private var amountIsFocused: Bool
+
     
-    var body: some View {
-        Text("Hello, world!")
-//        NavigationStack {
-//               Form {
-//                   Picker("Select your student", selection: $selectedStudent) {
-//                       ForEach(students, id: \.self) {
-//                           Text($0)
-//                       }
-//                   }
-//               }
-//        }
-//        Button("Tap Count: \(tapCount)") {
-//            self.tapCount += 1
-//        }
-//        Form {
-//            ForEach(0..<100) { number in
-//                Text("Row \(number)")
-//            }
-//        }
-//        Form {
-//            ForEach(0 ..< 100) {
-//                Text("Row \($0)")
-//            }
-//        }
-//        NavigationStack {
-//            Form {
-//                Section {
-//                    Text("Hello, world!")
-//                }
-//                TextField("Enter your name", text: $name)
-//                Text("Your name is \(name)")
-//            }
-//            .navigationTitle("SwiftUI")
-//            .navigationBarTitleDisplayMode(.inline)
-//        }
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    
+var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+
+                }
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                Picker("Number of people", selection: $numberOfPeople) {
+                       ForEach(2..<100) {
+                           Text("\($0) people")
+                       }
+                }
+                .pickerStyle(.navigationLink)
+                
+                Section("How much tip do you want to leave111?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
